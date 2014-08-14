66fc1e930d2b05e7f19a09123c1fafe861bc81fe
chris.schechter <chris.schechter@tubemogul.com>
2014-08-14 16:03:50 -0700

    updated models so we only have a pitches table

--- pitches/models.py
+++ pitches/models.py
@@ -5,24 +5,5 @@ class Pitch(models.Model):
 	pitch = models.CharField(max_length=200)
 	author = models.CharField(max_length=200)
 	date = models.DateTimeField('date pitched')
-
-	def __unicode__(self):
-		return self.pitch
-
-class Tag(models.Model):
-	tag = models.CharField(max_length=200)
-
-	def __unicode__(self):
-		return self.tag
-
-class PitchTag(models.Model):
-	pitch_id = models.ForeignKey(Pitch)
-	tag_id = models.ForeignKey(Tag)
-
-	def __unicode__(self):
-		pitch_id_ref = self(pitch_id)
-		tag_id_ref = sel
-
-class PitchKudo(models.Model):
-	kudo = models.IntegerField();
+	kudos = models.IntegerField();
 
