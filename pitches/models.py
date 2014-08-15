be17e152da002f2fd11068d62549fc320ec8c03e
chris.schechter <chris.schechter@tubemogul.com>
2014-08-14 18:20:00 -0700

    fixed merge error

--- pitches/models.py
+++ pitches/models.py
@@ -10,34 +10,9 @@ from django.utils import timezone
 class Pitch(models.Model):
 	pitch = models.CharField(max_length=200)
 	author = models.CharField(max_length=200)
-<<<<<<< HEAD
 	date = models.DateTimeField(auto_now_add=True)
 	kudos = models.IntegerField();
 	#objects = PitchManager()
-
-	def __unicode__(self):
-		return self.pitch
-
-class Tag(models.Model):
-	tag = models.CharField(max_length=200)
-
-	def __unicode__(self):
-		return self.tag
-
-class PitchTag(models.Model):
-	pitch_id = models.ForeignKey(Pitch)
-	tag_id = models.ForeignKey(Tag)
-
-	def __unicode__(self):
-		pitch_id_ref = self(pitch_id)
-		tag_id_ref = sel
-
-class PitchKudo(models.Model):
-	kudo = models.IntegerField();
-=======
-	date = models.DateTimeField('date pitched')
-	kudos = models.IntegerField();
->>>>>>> cf61da600b2561548f40dc5fc6de0d181a935813
 
 class PitchesModel:
 
