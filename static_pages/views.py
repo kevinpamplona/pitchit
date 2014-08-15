fe0f371a4e368263f9fa40051b3bf72be550e122
chris.schechter <chris.schechter@tubemogul.com>
2014-08-14 22:01:13 -0700

    assets work now muthafuckas

--- static_pages/views.py
+++ static_pages/views.py
@@ -14,3 +14,17 @@ def static_asset(request):
     response = HttpResponse(content=f.read(), content_type=_content_type, status=200)
     f.close()
     return response
+
+def static_page(request):
+    context = {}
+    if request.method == 'GET':
+        if request.user.is_authenticated():
+            context['logged_in'] = True
+        else:
+            context['logged_in'] = False
+    else:
+        # should not reach here
+        context['message'] = "Yo, you wrong!"
+
+    context['username'] = request.user.username 
+    return render(request, 'static_pages/index.html', context)
